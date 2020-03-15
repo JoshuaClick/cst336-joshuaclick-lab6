d874d1cc01a0ec50e26a550443258bbb9d738a05
JoshuaClick <59895515+JoshuaClick@users.noreply.github.com>
2020-03-15 16:18:47 -0700

    Added mercury, venus

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get("/", function(req, res){
     res.send("test");
 });
 
+app.get("/mercury", function(req, res){
+    res.send("Mercury page");
+});
+
+app.get("/venus", function(req, res){
+    res.send("Venus page");
+});
+
 app.listen("8081", "127.0.0.1", function(){
-    console.log("xpress Server is Running...");
+    console.log("Express Server is Running...");
 });
